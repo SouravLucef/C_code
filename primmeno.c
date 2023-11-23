554d74f2e41a8838e688f1511e2d0c49c19e2983
Lucefer256 <sm30122000@gmail.com>
2023-11-23 23:57:01 +0530

    added primenym

---
+++ primmeno.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the nomber");
+    scanf("%d",&n);
+    if(n<0){
+        printf("you have entered a negative number");
+
+    }
+    else {
+        printf("");
+
+    }
+    return 0;
+}
