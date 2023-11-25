e3b911a039b62bfeeb8b89fc5a7c880531eaad32
Lucefer256 <sm30122000@gmail.com>
2023-11-25 23:53:36 +0530

    modified primenum

--- primmeno.c
+++ primmeno.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    printf("Enter the nomber");
+    int n,i,c=0;
+    printf("Enter the number");
     scanf("%d",&n);
     if(n<0){
         printf("you have entered a negative number");
 
     }
     else {
-        printf("");
+        for( i=1;i<=n;i++){
+            if(n%i==0){
+                c=c+1;
+            }
+
+        }
+        if(c==2){
+            printf("the number is prime number");
+        }
+        else{
+        printf("The number is non prime number");
+        }
 
     }
     return 0;
