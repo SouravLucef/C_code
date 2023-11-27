fd8eced8bb002ae6e2d0f269703e378f0105d8c9
Lucefer256 <sm30122000@gmail.com>
2023-11-27 23:54:58 +0530

    reverse numnber

---
+++ reverse.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n,r,S=0,i;
+    printf("Enter num :");
+    scanf("%d",&n);
+
+    for (i=n;i>0;i--){
+        r=n%10;
+        S=S*10+r;
+        n=n/10;
+
+
+    }
+    printf("the reversed number is %d",S);
+
+    return 0;
+}
