53e8aa0a7e6c939a34c286cea6587796350ad817
Lucefer256 <sm30122000@gmail.com>
2023-12-01 23:58:36 +0530

    armstrong num

---
+++ armstrongnum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+    int n,i,r,s=0;
+    printf("Enter the number :  ");
+    scanf("%d",&n);
+
+    for (i=n;i>0;)
+    {
+        r=i%10;
+        s=s+(r*r);
+        i=i/10;
+    }
+
+    if(s==n)
+    {
+        printf("The number %d is a Armstrong number",n);
+
+    }
+    else{
+        printf("The Number %d is no an armstrong number. ",n);
+    }
+}
