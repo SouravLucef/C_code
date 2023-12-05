d8007a3c510506471974413fc2ef415f83f93d2a
Lucefer256 <sm30122000@gmail.com>
2023-12-05 23:59:55 +0530

    new

--- listdispuserinput.c
+++ listdispuserinput.c
@@ -2,13 +2,16 @@
 void main()
 {
     int n,i, data[n];
+    printf("Enter the num of elements : \n");
+    scanf("%d",n);
+
     printf("Enter the elements : \n");
-    for(i=0;i<5;i++)
+    for(i=0;i<n;i++)
     {
         scanf("%d",&data[i]);
     }
     printf("The Output list is : " );
-    for (i=0;i<5;i++){
+    for (i=0;i<n;i++){
     printf(" %d",data[i]);
     }
 }
