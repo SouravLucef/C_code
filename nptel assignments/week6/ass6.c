6cbd3e56e8580d31eb10d000bcd55382e959f35e
Lucefer256 <sm30122000@gmail.com>
2024-03-13 18:51:44 +0530

    Ass6

--- nptel assignments/week6/ass6.c
+++ nptel assignments/week6/ass6.c
@@ -15,9 +15,32 @@ void Q_10(){
     printf("i=%d,j=%d,k=%d",i,j,k);
 
 }
+void P1()
+{
 
+    int i, n, largest;
+    int arr[100];
+
+    scanf("%d", &n); /*Accepts total number of elements from the test data */
+
+    for (i = 0; i < n; ++i)
+    {
+        scanf("%d", &arr[i]); /* Accepts the array element from test data */
+    }
+    largest = arr[0];
+
+    for (i = 0; i < n; i++)
+    {
+        if ( arr[i]>=largest)
+        {
+            largest = arr[i];
+        }
+    }
+    printf("Largest element=%d", largest);
+}
 
 void main(){
     // mAtrix();
-    Q_10();
+    //Q_10();
+    P1();
 }
